9cb4f694011116a1ebe966aaff2f6c29d820af82
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-05-20 23:20:21 +0330

    add some env key (GB_Products_limit, GB_Products_sortBy, GB_Products_sortType)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,9 @@
 interface ImportMetaEnv {
 	readonly GB_BASE_URL: string;
 	readonly GB_TIMEOUT: string;
+	readonly GB_Products_limit: string;
+	readonly GB_Products_sortBy: string;
+	readonly GB_Products_sortType: string;
 }
 
 interface ImportMeta {
