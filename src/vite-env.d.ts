b645bd507c2eb7bfd13fe0e32e3cc529914c599e
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-05-20 16:50:53 +0330

    add type for env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	readonly GB_BASE_URL: string;
+	readonly GB_TIMEOUT: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
