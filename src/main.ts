07364543195acb9fefb95d42868f6feda4730b94
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-05-21 19:03:33 +0330

    install & config pinia

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import '~src/assets/styles/index.scss';
 
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
 import App from '~src/App.vue';
 
 import { router } from './routes';
 
+const pinia = createPinia();
 const app = createApp(App);
 
+app.use(pinia);
 app.use(router);
+
 app.mount('#app');
